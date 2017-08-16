4b85876cedc171e4db991a8d3af30c946dbd4b27
matthaywardwebdesign <matt@matthayward.com.au>
2017-08-16 21:43:45 +1000

    Finished implementation and added documentation

--- src/LinkedListMultiset.java
+++ src/LinkedListMultiset.java
@@ -3,34 +3,50 @@ import java.util.*;
 
 public class LinkedListMultiset<T> extends Multiset<T>
 {
+	/* Define variables to store references to the head node and tail node */
 	private Node<T> head;
-	private Node<T> current;
+	private Node<T> tail;
 
 	public LinkedListMultiset() {}
 
+	/* Adds an item to the LinkedListMultiset */
 	public void add(T item) {
+		/* Checks to see whether this is the first item in the list */
 		if ( head == null ) {
+			/* Creates a new node with no head or tail */
 			Node<T> node = new Node( item );
-			head = node;
-			current = node;
+			/* Set the head and current node variables to the newly created node */
+			this.head = node;
+			this.tail = node;
 			return;
 		}
 
+		/* Check whether this item already exists in the LinkedListMultiset */
 		Node<T> existing = this.find( item );
 
+		/* If it does exist increment the count */
 		if ( existing != null ) {
 			existing.incrementCount();
 			return;
 		}
 
-		Node<T> node = new Node( this.current, null, item );
-		this.current.setTail( node );
-		this.current = node;
+		/**
+		 * Item doesn't exist in the LinkedListMultiset, create a new node with
+		 * this item, set the tail of the current tail node to reference the newly
+		 * created node and set the tail of the LinkedListMultiset to point to it.
+		 */
+		Node<T> node = new Node( this.tail, null, item );
+		this.tail.setTail( node );
+		this.tail = node;
 	}
 
 
+	/* Returns the number of the provided item in the LinkedListMultiset */
 	public int search(T item) {
+		/* Attempt to find the node which contains this item in the list */
 		Node<T> node = this.find( item );
+
+		/* If the node exists return the count, otherwise return 0 */
 		if (node != null ) {
 			return node.getCount();
 		}
@@ -38,27 +54,35 @@ public class LinkedListMultiset<T> extends Multiset<T>
 		return 0;
 	}
 
+	/* Attempts to find the node which contains a particular item, otherwise returns null */
 	public Node<T> find(T item) {
-		Node<T> position = head;
+		/**
+		 * Create a variable to store the current position as we search and set it
+		 * to be equal to current head of the LinkedListMultiset
+		 */
+		Node<T> position = this.head;
 
-		while ( position.getTail() != null ) {
+		/* Continue iterating through the list until their is no more nodes to access */
+		while ( position != null ) {
+			/* Check whether this node contains the item, if it does return the node */
 			if ( position.getData() == item || position.getData().equals( item )) {
 				return position;
 			}
 
+			/* This node doesn't contain the item, move onto the next node */
 			position = position.getTail();
 		}
 
-		if ( position != null && ( position.getData() == item || position.getData().equals( item )) ) {
-			return position;
-		}
-
+		/* We weren't able to find the node that contains the item, return null */
 		return null;
 	}
 
-
+	/* Removes one of the items from the list if it exists */
 	public void removeOne(T item) {
+		/* Attempt to find the node that contains this item */
 		Node<T> node = this.find( item );
+
+		/* If the node exists, decrement the count and remove the node if the count = 0 */
 		if (node != null ) {
 			node.decrementCount();
 
@@ -69,47 +93,62 @@ public class LinkedListMultiset<T> extends Multiset<T>
 	}
 
 
+	/* Removes all of the items that are equal to the provided item */
 	public void removeAll(T item) {
+		/* Attempt to find the node that contains instances of this item */
 		Node<T> node = this.find( item );
+
+		/* If this node doesn't exist return null */
 		if ( node == null ) {
 			return;
 		}
 
+		/* Get the head and the tail of the node associated with this item */
 		Node<T> head = node.getHead();
 		Node<T> tail = node.getTail();
 
+		/* Update the head of the tail node to be the head of the node associated with this item */
 		if ( tail != null ) {
 			tail.setHead( head );
 		}
 
+		/* Update the tail of the head node to be the tail of the node associated with this item */
 		if ( head != null ) {
 			head.setTail( tail );
 		}
 
+		/**
+		 * If this node is the first node in the list (the head) update the head
+		 * of the list to be the tail of the node associated with this item
+		 */
 		if ( node == this.head ) {
 			this.head = tail;
 		}
 
-		if ( node == this.current ) {
-			this.current = head;
+		/**
+		 * If this node is the last node in the list (the tail) update the tail
+		 * of the list to be the head of the node associated with this item
+		 */
+		if ( node == this.tail ) {
+			this.tail = head;
 		}
 	}
 
-
+	/* Prints a list of items in the LinkedListMultiset and their counts */
 	public void print(PrintStream out) {
+		/**
+		 * Create a variable to store the current position as we search and set it
+		 * to be equal to current head of the LinkedListMultiset
+		 */
 		Node<T> position = head;
 
-		if ( position == null ) {
-			return;
-		}
-
-		while ( position.getTail() != null ) {
+		/* Continue iterating through the list until their is no more nodes to access */
+		while ( position != null ) {
+			/* Print the node's item and the associated count */
 			out.printf( "%s | %d\n", position.getData(), position.getCount());
+
+			/* Move onto the next item */
 			position = position.getTail();
-		}
-
-		if ( position != null ) {
-			out.printf( "%s | %d\n", position.getData(), position.getCount());
 		}
 	}
 
