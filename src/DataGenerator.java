6de427025475f5bf8b126fc950fd20e95d18cfa6
zcallan <kaln.delbridge@hotmail.com>
2017-08-30 23:05:11 +1000

    added classList to data generator

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -3,27 +3,44 @@ import java.io.*;
 
 
 public class DataGenerator {
-  private List<String> shoppingList = new ArrayList<String>();
   private String groceriesFileName = "groceries.txt";
+  private String classListFileName = "class-list.txt";
   
   public DataGenerator() {}
   
   public List<String> getShoppingList() throws IOException {
     FileInputStream inputStream = new FileInputStream( this.groceriesFileName );
     BufferedReader br = new BufferedReader( new InputStreamReader( inputStream ));
+    List<String> shoppingList = new ArrayList<String>();
     
     try {
       String line;
       
       while (( line = br.readLine()) != null ) {
-        this.shoppingList.add( line );
+        shoppingList.add( line );
       }
     } finally {
       br.close();
     }
     
-    Collections.shuffle( this.shoppingList );
+    return shoppingList;
+  }
+  
+  public List<String> getClassList() throws IOException {
+    FileInputStream inputStream = new FileInputStream( this.classListFileName );
+    BufferedReader br = new BufferedReader( new InputStreamReader( inputStream ));
+    List<String> classList = new ArrayList<String>();
     
-    return this.shoppingList;
+    try {
+      String line;
+      
+      while (( line = br.readLine()) != null ) {
+        classList.add( line );
+      }
+    } finally {
+      br.close();
+    }
+    
+    return classList;
   }
 }
--- src/ReportEvaluator.java
+++ src/ReportEvaluator.java
@@ -3,17 +3,22 @@ import java.util.*;
 
 public class ReportEvaluator {
   protected static final String progName = "ReportEvaluator";
+  protected static DataGenerator generator = new DataGenerator();
   
   public static void printUsage() {
-    System.err.println( progName + ": <scenario> = <shoppingList | goFuckYourself>" );
+    System.err.println( progName + ": <scenario> = <shoppingList | classList>" );
     System.exit( 1 );
   }
   
-  public static void evaluateShoppingList() {
-    DataGenerator generator = new DataGenerator();
-    
+  public static void evaluateShoppingList( int multiplesOfArray ) {
     try {
       List<String> groceryList = generator.getShoppingList();
+  
+      for ( int i = 0; i < multiplesOfArray; i++ ) {
+        groceryList.addAll( generator.getShoppingList() );
+      }
+      
+      Collections.shuffle( groceryList );
       
       for ( String item : groceryList ) {
         System.out.println( item );
@@ -23,17 +28,40 @@ public class ReportEvaluator {
     }
   }
   
+  public static void evaluateClassList( int multiplesOfArray ) {
+    try {
+      List<String> classList = generator.g();
+      
+      for ( int i = 0; i < multiplesOfArray; i++ ) {
+        classList.addAll( generator.getClassList() );
+      }
+  
+      Collections.shuffle( classList );
+  
+      for ( String item : classList ) {
+        System.out.println( item );
+      }
+    } catch (Exception e) {
+      System.out.println( e );
+    }
+  }
+  
   public static void main( String[] args ) {
-    if ( args.length != 1 ) {
+    if ( args.length != 2 ) {
       System.err.println( "Incorrect number of arguments." );
       printUsage();
     }
     
     String scenario = args[0];
+    int multiplesOfArray = Integer.parseInt( args[1] );
     
     switch ( scenario ) {
       case "shoppingList":
-        evaluateShoppingList();
+        evaluateShoppingList( multiplesOfArray );
+        break;
+  
+      case "classList":
+        evaluateClassList( multiplesOfArray );
         break;
       
       default:
